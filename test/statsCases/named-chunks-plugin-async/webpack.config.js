4ecc2959a02d4f56d3809f4f2b9d21e970541025
Tobias Koppers <tobias.koppers@googlemail.com>
2018-08-13 18:05:20 +0200

    WIP

--- test/statsCases/named-chunks-plugin-async/webpack.config.js
+++ test/statsCases/named-chunks-plugin-async/webpack.config.js
@@ -2,6 +2,7 @@
 
 const NamedChunksPlugin = require("../../../lib/NamedChunksPlugin");
 const RequestShortener = require("../../../lib/RequestShortener");
+const { compareModulesById } = require("../../../lib/util/comparators");
 
 module.exports = {
 	mode: "production",
@@ -15,10 +16,13 @@ module.exports = {
 				return chunk.name;
 			}
 			const chunkModulesToName = chunk =>
-				Array.from(chunkGraph.getChunkModulesIterable(chunk), mod => {
-					const rs = new RequestShortener(mod.context);
-					return rs.shorten(mod.request).replace(/[./\\]/g, "_");
-				}).join("-");
+				Array.from(
+					chunkGraph.getOrderedChunkModulesIterable(chunk, compareModulesById),
+					mod => {
+						const rs = new RequestShortener(mod.context);
+						return rs.shorten(mod.request).replace(/[./\\]/g, "_");
+					}
+				).join("-");
 
 			if (chunkGraph.getNumberOfChunkModules(chunk) > 0) {
 				return `chunk-containing-${chunkModulesToName(chunk)}`;
